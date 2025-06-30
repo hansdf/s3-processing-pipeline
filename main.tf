d155074f27204dbfceefef93d068dfd445be9a44
hansdf <dornsback@gmail.com>
2025-06-30 18:42:28 -0300

    reorganizing

--- main.tf
+++ main.tf
@@ -4,9 +4,24 @@ provider "aws" {
 
 # two S3 buckets, one for input that will trigger the initial workflow, and another for output processed files
 resource "aws_s3_bucket" "input_bucket" {
-  bucket = "input_bucket_s3_img_processing_pipeline"
+  bucket = "input-bucket-s3-img-processing-pipeline"
+}
+resource "aws_s3_bucket" "output_bucket" {
+  bucket = "output-bucket-s3-img-processing-pipeline" 
 }
 
-resource "aws_s3_bucket" "output_bucket" {
-  bucket = "output_bucket_s3_img_processing_pipeline" 
+# IAM role for lambda func
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "serverless_processing_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
 }
