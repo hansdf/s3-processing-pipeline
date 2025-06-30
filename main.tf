8b69fccff416e75bb9d19393c0bd50e4de146bea
hansdf <dornsback@gmail.com>
2025-06-30 18:13:41 -0300

    py script update

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 
 # two S3 buckets, one for input that will trigger the initial workflow, and another for output processed files
 resource "aws_s3_bucket" "input_bucket" {
-  bucket = "PLACEHOLDER" 
+  bucket = "input_bucket_s3_img_processing_pipeline"
 }
 
 resource "aws_s3_bucket" "output_bucket" {
-  bucket = "PLACEHOLDER" 
+  bucket = "output_bucket_s3_img_processing_pipeline" 
 }
